e2925f8e9896663a4b0e2e22dbf2631bb0f921dd
Lzybear22 <nes23003@byui.edu>
2024-04-27 14:50:09 -0600

    learning activity prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,75 @@
 using System;
+using System.Data;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int answer = int.Parse(grade);
+
+        string letter = "";
+        string sign = "";
+
+    if (answer >= 90)
+    {
+        letter = "A";
+    }
+    else if (answer >= 80)
+    {
+        letter = "B";
+    }
+    else if (answer >= 70)
+    {
+        letter = "C";
+    }
+    else if (answer >= 60)
+    {
+        letter = "D";
+    }
+    else
+    {
+        letter = "F";
+    }
+
+    int lastdigit = answer % 10;
+    if (lastdigit >= 7)
+    {
+        sign = "+";
+    }
+
+    else if (lastdigit < 3)
+    {
+        sign = "-";
+    }
+    else
+    {
+        sign = "";
+    }
+    
+    if (letter == "A" && sign == "+")
+    {
+        sign = "";
+    }
+    else if (letter == "F" && (sign == "+" || sign == "-"))
+    {
+        sign = "";
+    }
+
+    Console.WriteLine($"Your grade will be {letter}{sign}. ");
+
+    if (answer >= 70)
+    {
+        Console.WriteLine("You passed! ");
+    }
+    else
+    {
+        Console.WriteLine("You Failed! ");
+    }
+
+
     }
 }
