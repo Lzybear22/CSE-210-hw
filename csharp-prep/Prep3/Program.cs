22f62865a2a24ed2d6133b3de1f23bc56e8b0642
Lzybear22 <nes23003@byui.edu>
2024-05-01 15:25:57 -0600

    magic number guess

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomnumber = new Random();
+        int magicnumber = randomnumber.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != magicnumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicnumber > guess)
+            {
+                Console.WriteLine("Higher.");
+            }
+            else if (magicnumber < guess)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed right! ");
+            }
+        }
     }
 }
