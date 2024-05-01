79171c8ff9c436ea9a9d6afe7686b11cad05422a
Lzybear22 <nes23003@byui.edu>
2024-05-01 15:46:09 -0600

    prep 4 lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = [];
+
+        Console.WriteLine("Enter a series of numbers (enter 0 to stop): ");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int number))
+            {
+                if (number == 0)
+                    break;
+
+                numbers.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        int sum = numbers.Sum();
+
+        double average = numbers.Count > 0 ? numbers.Average() : 0;
+
+        int max = numbers.Count > 0 ? numbers.Max() : 0;
+
+        Console.WriteLine($"Sum of the numbers: {sum}");
+        Console.WriteLine($"Average of the numbers: {average}");
+        Console.WriteLine($"Maximum number in the list: {max}");
     }
 }
