bdfa26ac16b3f504e4fea05ecba0f9686057f549
Lzybear22 <nes23003@byui.edu>
2024-05-13 19:22:18 -0600

    Program.cs working options

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -3,18 +3,31 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+{
     int userchoice = 1;
-    while (userchoice != 1)
     
-
-      Console.WriteLine("Write New Entry");
-      Console.WriteLine("Display Journal");
-      Console.WriteLine("Save Journal");
-      Console.WriteLine("Load Journal");
-      Console.WriteLine("0 To End Program");
-    userchoice = int.Parse("Please pick a option");
+    while (userchoice != 0)
+    {
+    Console.WriteLine("Please pick an option");
+    Console.WriteLine("Write New Entry");
+    Console.WriteLine("Display Journal");
+    Console.WriteLine("Save Journal");
+    Console.WriteLine("Load Journal");
+    Console.WriteLine("0 To End Program");
     
+    Console.Write("Enter your choice: ");
+    string userInput = Console.ReadLine();
+    
+    if (!int.TryParse(userInput, out userchoice))
+    {
+        Console.WriteLine("Invalid input. Please enter a valid number. ");
+        continue;
+    }
 
+    Console.WriteLine("You chose: " + userchoice);
+
+    
+    Console.WriteLine("Journal closed. ");
+        }
     }
 }
