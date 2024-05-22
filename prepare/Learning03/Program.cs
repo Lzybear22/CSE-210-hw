5add205e79b60be029f6c86d46a418ed7005b1fd
Lzybear22 <nes23003@byui.edu>
2024-05-22 15:18:24 -0600

    prepare 3 finished

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+    Fraction frac1 = new Fraction();
+    Console.WriteLine(frac1.GetDecimalValue());
+    Console.WriteLine(frac1.GetFractionString());
+
+    Fraction frac2 = new Fraction(5);
+    Console.WriteLine(frac2.GetDecimalValue());
+    Console.WriteLine(frac2.GetFractionString());
+
+    Fraction frac3 = new Fraction(3,4);
+    Console.WriteLine(frac3.GetDecimalValue());
+    Console.WriteLine(frac3.GetFractionString());
+
+    Fraction frac4 = new Fraction(1, 3);
+    Console.WriteLine(frac4.GetDecimalValue());
+    Console.WriteLine(frac4.GetFractionString());
+
     }
 }
